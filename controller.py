7931806ec8e88f0962c213df7eedc764371767a1
Bryan Burgers <bryan.burgers@gmail.com>
2009-05-01 08:40:27 -0500

    It works! Updating to Twitter works through /util/update

--- controller.py
+++ controller.py
@@ -1,8 +1,11 @@
+import base64
 import cgi
 import os
 
 import datetime
+import urllib
 
+from google.appengine.api import urlfetch
 from google.appengine.api import users
 from google.appengine.ext import db
 from google.appengine.ext import webapp
@@ -43,7 +46,7 @@ class TweetsPage(webapp.RequestHandler):
 	def get(self, profileName):
 		profile = db.GqlQuery("SELECT * FROM Profile WHERE name = :1", profileName).get()
 
-		tweets = db.GqlQuery("SELECT * FROM Tweet WHERE profile = :1 ORDER BY date", profile)
+		tweets = db.GqlQuery("SELECT * FROM Tweet WHERE profile = :1 AND posted = :2 ORDER BY date", profile, False)
 
 		template_values = {
 				"profile": profile,
@@ -66,19 +69,71 @@ class TweetsPage(webapp.RequestHandler):
 
 		self.redirect('/tweets/' + profile.name)
 
+class TweetPage(webapp.RequestHandler):
+	def get(self, key):
+		tweet = db.get(db.Key(key))
+
+		template_values = {
+				"profile": tweet.profile,
+				"tweet": {
+					"text": tweet.text,
+					"key": str(tweet.key()),
+					"date": tweet.date.strftime("%Y-%m-%d %H:%M"),
+				},
+		}
+
+		path = os.path.join(os.path.dirname(__file__), 'tweet.html')
+		self.response.out.write(template.render(path, template_values))
+
+	def post(self, key):
+		tweet = db.get(db.Key(key))
+
+		if not self.request.get("Delete", default_value=None) == None:
+			tweet.delete()
+			self.redirect('/tweets/' + tweet.profile.name)
+		else:
+			tweet.text = self.request.get("text")
+			tweet.date = datetime.datetime.strptime(self.request.get("date"), "%Y-%m-%d %H:%M")
+			tweet.put()
+			self.redirect('/tweets/' + tweet.profile.name)
+
 class UpdatePage(webapp.RequestHandler):
 	def get(self):
 
-		tweets = db.GqlQuery("SELECT * FROM Tweet WHERE date < :1 ORDER BY date", datetime.datetime.utcnow())
+		tweets = db.GqlQuery("SELECT * FROM Tweet WHERE date < :1 AND posted = :2 ORDER BY date", datetime.datetime.utcnow(), False)
 
 		self.response.headers["Content-Type"] = "text/plain"
 
+		url = "http://twitter.com/statuses/update.json"
+		
+
 		for tweet in tweets:
-			self.response.out.write("Date: " + str(tweet.date) + "\n")
 			self.response.out.write("Text: " + str(tweet.text) + "\n")
 			self.response.out.write("Login: " + str(tweet.profile.name) + "\n")
 			self.response.out.write("Password: " + str(tweet.profile.password) + "\n")
 			self.response.out.write("Updated: " + str(tweet.postedDate) + "\n")
+			try:
+				encodedAuthentication = base64.encodestring(tweet.profile.name + ":" + tweet.profile.password).strip()
+				form_fields = {
+	  				"status": tweet.text,
+				}
+				form_data = urllib.urlencode(form_fields)
+				result = urlfetch.fetch(
+						url = url,
+						payload = form_data,
+						method = urlfetch.POST,
+						headers = {
+							#'Content-Type': 'application/x-www-form-urlencoded',
+							'Authorization': 'Basic ' + encodedAuthentication})
+				if result.status_code == 200:
+					tweet.posted = True
+					tweet.postedDate = datetime.datetime.utcnow()
+					tweet.put()
+				else:
+					self.response.out.write("FAILED! Code: " + str(result.status_code) + "\n")
+					self.response.out.write(result.content + "\n")
+			except Exception, e:
+				self.response.out.write("FAILED! " + str(e) + "\n")
 			self.response.out.write("\n")
 
 	def post(self):
@@ -362,6 +417,7 @@ application = webapp.WSGIApplication(
 		('/', ProfilesPage),
 		('/profiles', ProfilesPage),
 		('/tweets/([^/]*)', TweetsPage),
+		('/tweet/([^/]*)', TweetPage),
 		('/util/update', UpdatePage),
 #		('/wedding/guests(?:\.xml)?', Guests),
 #		('/wedding/guest/([^/]*)', GuestR),
